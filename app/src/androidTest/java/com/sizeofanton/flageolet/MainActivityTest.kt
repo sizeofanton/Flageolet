35a0fe97a8e20b84bbecda3bbf49c2a7b857e261
Anton Sizov <sizeofanton@gmail.com>
2020-06-04 22:30:34 +0300

    MainActivityTest - instrumental test added

--- app/src/androidTest/java/com/sizeofanton/flageolet/MainActivityTest.kt
+++ app/src/androidTest/java/com/sizeofanton/flageolet/MainActivityTest.kt
@@ -1,24 +1,41 @@
 package com.sizeofanton.flageolet
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.filters.LargeTest
+import org.hamcrest.Matchers.not
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+@LargeTest
+class MainActivityTest {
+
+    @get:Rule var activityScenarioRule = activityScenarioRule<MainActivity>()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.sizeofanton.flageolet", appContext.packageName)
+    fun test_noteStartUp() {
+        onView(withId(R.id.tvNote)).check(matches(withText("?")))
     }
+
+    @Test
+    fun test_frequencyStartUp() {
+        onView(withId(R.id.tvFreq)).check(matches(withText("0 Hz")))
+    }
+
+    @Test
+    fun test_noteDeviationViewShown() {
+        onView(withId(R.id.noteDeviationView)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_radioGroupIsHidden() {
+        onView(withId(R.id.rgStrings)).check(matches(not(isDisplayed())))
+    }
+
 }
