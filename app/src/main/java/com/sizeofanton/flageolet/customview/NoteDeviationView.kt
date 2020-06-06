f134a2b74e0462922702efd9b175819ae13d4f59
Anton Sizov <sizeofanton@gmail.com>
2020-06-06 12:11:33 +0300

    NoteDeviationView orientation added, /res/values/attrs.xml with NoteDeviationView xml attributes added

--- app/src/main/java/com/sizeofanton/flageolet/customview/NoteDeviationView.kt
+++ app/src/main/java/com/sizeofanton/flageolet/customview/NoteDeviationView.kt
@@ -4,13 +4,30 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import com.sizeofanton.flageolet.R
 import java.lang.Exception
 
 
-class NoteDeviationView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttrs: Int = 0) : View(context, attrs, defStyleAttrs){
+class NoteDeviationView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttrs: Int = 0) : View(context, attrs, defStyleAttrs){
+
+    private var orientation: Int = 0
+
+    init {
+        if (attrs != null) {
+            val fromXml = getContext().obtainStyledAttributes(attrs, R.styleable.NoteDeviationView)
+            orientation = fromXml.getInt(R.styleable.NoteDeviationView_orientation, 0)
+            fromXml.recycle()
+        }
+    }
+
 
     private val labels = arrayOf("+50", "+40", "+30", "+20", "+10", "  0",
-                                              "-10", "-20", "-30", "-40", "-50")
+                                           "-10", "-20", "-30", "-40", "-50")
+    private val labelsH = arrayOf("-50", "-40", "-30", "-20", "-10", "  0",
+                                              "+10", "+20", "+30", "+40", "+50")
 
     private var primaryLineWidth = 20.0f
     private var secondaryLineWidth = 10.0f
@@ -80,18 +97,37 @@ class NoteDeviationView @JvmOverloads constructor(context: Context, attrs: Attri
     }
 
     override fun onDraw(canvas: Canvas) {
-        primaryLineWidth = height / 107.5f
-        secondaryLineWidth = height / 215.0f
+        recalculateLineWidth()
         reloadPaints()
-        drawOutline(canvas)
-        drawFrontierSectors(canvas)
-        drawCentralSector(canvas)
-        drawMarks(canvas)
-        drawText(canvas)
-        if (pointerVisible) drawPointer(canvas)
+        if (orientation == 0) {
+            drawOutline(canvas)
+            drawFrontierSectors(canvas)
+            drawCentralSector(canvas)
+            drawMarks(canvas)
+            drawText(canvas)
+            if (pointerVisible) drawPointer(canvas)
+        } else {
+            drawOutlineH(canvas)
+            drawFrontierSectorH(canvas)
+            drawCentralSectorH(canvas)
+            drawMarksH(canvas)
+            drawTextH(canvas)
+            drawPointerH(canvas)
+            if (pointerVisible) drawPointerH(canvas)
+        }
+
         super.onDraw(canvas)
     }
 
+    private fun recalculateLineWidth() {
+        if (orientation == 0) {
+            primaryLineWidth = height / 107.5f
+            secondaryLineWidth = height / 215.0f
+        } else {
+            primaryLineWidth = height / 53.75f
+            secondaryLineWidth = height / 107.5f
+        }
+    }
 
     private fun drawOutline(canvas: Canvas) {
         val rect = Rect(
@@ -99,6 +135,16 @@ class NoteDeviationView @JvmOverloads constructor(context: Context, attrs: Attri
                 (height.toFloat() / 22).toInt(),
                 (9 * width.toFloat() / 11).toInt(),
                 (21 * height.toFloat() / 22).toInt()
+        )
+        canvas.drawRect(rect, outlinePaint)
+    }
+
+    private fun drawOutlineH(canvas: Canvas) {
+        val rect = Rect(
+            (width.toFloat() / 22).toInt(),
+            (height.toFloat() / 11).toInt(),
+            (21 * width.toFloat() / 22).toInt(),
+            (9 * height.toFloat() / 11).toInt()
         )
         canvas.drawRect(rect, outlinePaint)
     }
@@ -113,12 +159,32 @@ class NoteDeviationView @JvmOverloads constructor(context: Context, attrs: Attri
         )
     }
 
+    private fun drawCentralSectorH(canvas: Canvas) {
+        canvas.drawLine(
+            11 * width.toFloat() / 22,
+            height.toFloat() / 11,
+            11 * width.toFloat() / 22,
+            9 * height.toFloat() / 11,
+            centralPaint
+        )
+    }
+
     private fun drawFrontierSectors(canvas: Canvas) {
         val rect = Rect(
                 (width.toFloat() / 11).toInt(),
                 (9 * height.toFloat() / 22).toInt(),
                 (9 * width.toFloat() / 11).toInt(),
                 (13 * height.toFloat() / 22).toInt()
+        )
+        canvas.drawRect(rect, frontierPaint)
+    }
+
+    private fun drawFrontierSectorH(canvas: Canvas) {
+        val rect = Rect(
+            (9 * width.toFloat() / 22).toInt(),
+            (1 * height.toFloat() / 11).toInt(),
+            (13 * width.toFloat() / 22).toInt(),
+            (9 * height.toFloat() / 11).toInt()
         )
         canvas.drawRect(rect, frontierPaint)
     }
@@ -182,10 +248,76 @@ class NoteDeviationView @JvmOverloads constructor(context: Context, attrs: Attri
 
     }
 
+    private fun drawMarksH(canvas: Canvas) {
+        for (i in 0..9) {
+            canvas.drawLine(
+                (2 + 2 * i) * width.toFloat() / 22,
+                height.toFloat() / 11,
+                (2 + 2 * i) * width.toFloat() / 22,
+                2f * height.toFloat() / 11,
+                markPaint
+            )
+        }
+        for (i in 0..9) {
+            canvas.drawLine(
+                (2 + 2 * i) * width.toFloat() / 22,
+                8f * height.toFloat() / 11,
+                (2 + 2 * i) * width.toFloat() / 22,
+                9f * height.toFloat() / 11,
+                markPaint
+            )
+        }
+        for (i in 1..10) {
+            canvas.drawLine(
+                (2f * i - 0.5f) * width.toFloat() / 22,
+                height.toFloat() / 11,
+                (2f * i - 0.5f) * width.toFloat() / 22,
+                1.5f * height.toFloat() / 11,
+                markPaint
+            )
+        }
+
+        for (i in 1..10) {
+            canvas.drawLine(
+                (2f * i - 0.5f) * width.toFloat() / 22,
+                8.5f * height.toFloat() / 11,
+                (2f * i - 0.5f) * width.toFloat() / 22,
+                9f * height.toFloat() / 11,
+                markPaint
+            )
+        }
+
+        for (i in 1..10) {
+            canvas.drawLine(
+                (2f * i + 0.5f) * width.toFloat() / 22,
+                height.toFloat() / 11,
+                (2f * i + 0.5f) * width.toFloat() / 22,
+                1.5f * height.toFloat() / 11,
+                markPaint
+            )
+        }
+
+        for (i in 1..10) {
+            canvas.drawLine(
+                (2f * i + 0.5f) * width.toFloat() / 22,
+                8.5f * height.toFloat() / 11,
+                (2f * i + 0.5f) * width.toFloat() / 22,
+                9f * height.toFloat() / 11,
+                markPaint
+            )
+        }
+    }
+
     private fun drawText(canvas: Canvas) {
         fontPaint.textSize = height / 42f
         for (i in labels.indices)
-           canvas.drawText(labels[i], 10f * width / 11, (1f + 2f * i) * height / 22, fontPaint)
+           canvas.drawText(labels[i], 9.5f * width / 11, (1f + 2f * i) * height / 22, fontPaint)
+    }
+
+    private fun drawTextH(canvas: Canvas) {
+        fontPaint.textSize = height / 21f
+        for (i in labelsH.indices)
+            canvas.drawText(labels[i], (0.75f + 2f*i) * width / 22, 10f * height / 11, fontPaint)
     }
 
     private fun drawPointer(canvas: Canvas) {
@@ -195,6 +327,16 @@ class NoteDeviationView @JvmOverloads constructor(context: Context, attrs: Attri
                 9.0f * width.toFloat() / 11,
                 pointerPosition * height,
                 pointerPaint
+        )
+    }
+
+    private fun drawPointerH(canvas: Canvas) {
+        canvas.drawLine(
+            pointerPosition * width,
+            height.toFloat() / 11,
+            pointerPosition * width,
+            9f * height / 11,
+            pointerPaint
         )
     }
 
