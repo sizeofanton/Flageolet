95901012c62b407a596029b47f3dbb86a594784d
Anton Sizov <sizeofanton@gmail.com>
2020-06-07 17:16:48 +0300

    MainActivityTest - few test cases added

--- app/src/androidTest/java/com/sizeofanton/flageolet/MainActivityTest.kt
+++ app/src/androidTest/java/com/sizeofanton/flageolet/MainActivityTest.kt
@@ -1,5 +1,6 @@
 package com.sizeofanton.flageolet
 
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -7,7 +8,7 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import org.hamcrest.Matchers.not
+import org.hamcrest.Matchers.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,22 +21,46 @@ class MainActivityTest {
 
     @Test
     fun test_noteStartUp() {
-        onView(withId(R.id.tvNote)).check(matches(withText("?")))
+        onView(withId(R.id.tvNote)).
+            check(matches(withText("?")))
     }
 
     @Test
     fun test_frequencyStartUp() {
-        onView(withId(R.id.tvFreq)).check(matches(withText("0 Hz")))
+        onView(withId(R.id.tvFreq))
+            .check(matches(withText("0 Hz")))
     }
 
     @Test
     fun test_noteDeviationViewShown() {
-        onView(withId(R.id.noteDeviationView)).check(matches(isDisplayed()))
+        onView(withId(R.id.noteDeviationView))
+            .check(matches(isDisplayed()))
     }
 
     @Test
     fun test_radioGroupIsHidden() {
-        onView(withId(R.id.rgStrings)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.rgStrings))
+            .check(matches(not(isDisplayed())))
+    }
+
+    @Test
+    fun test_radioGroupIsVisible() {
+        onView(withId(R.id.spinnerNotes))
+            .perform(click())
+        onData(allOf(`is`(instanceOf(String::class.java)), `is`("Standard Tuning (EBGDAE)")))
+            .perform(click())
+        onView(withId(R.id.rgStrings))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_spinnerItemChanged() {
+        onView(withId(R.id.spinnerNotes))
+            .perform(click())
+        onData(allOf(`is`(instanceOf(String::class.java)), `is`("Standard Tuning (EBGDAE)")))
+            .perform(click())
+        onView(withId(R.id.spinnerNotes))
+            .check(matches(withSpinnerText(containsString("Standard Tuning (EBGDAE)"))))
     }
 
 }
